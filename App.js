0d99d083665eb156a8ea34d7a0d237feab7f2d3c
kritipriya12 <kritipriya1206@gmail.com>
2025-03-11 22:25:34 +0530

    css

--- App.js
+++ App.js
@@ -1,3 +1,3 @@
-const heading = React.createElement("h1",{}, "Hello world from React");
+const heading = React.createElement("h1",{id:"heading", xyz:"abc"}, "Hello world from React");
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading);
